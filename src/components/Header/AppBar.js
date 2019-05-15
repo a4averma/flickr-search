3c373d014fccd45b7da69a80cfb66320254021d5
Aditya Verma <siradityaverma@gmail.com>
2019-05-15 11:16:40 +0530

    Search Input Is Now Centre

--- src/components/Header/AppBar.js
+++ src/components/Header/AppBar.js
@@ -1,19 +1,18 @@
-import React from 'react';
-import { Box } from 'grommet';
-
+import React from "react";
+import { Box } from "grommet";
 
 function AppBar(props) {
   return (
     <Box
-     tag='header'
-     direction='row'
-     align='center'
-     justify='between'
-     background='light-2'
-     pad={{ vertical: 'small', horizontal: 'medium' }}
-     elevation='medium'
-     {...props}
-  />
+      tag="header"
+      direction="row"
+      align="center"
+      justify="between"
+      background="light-2"
+      pad={{ vertical: "small", horizontal: "medium" }}
+      elevation="medium"
+      {...props}
+    />
   );
 }
 
